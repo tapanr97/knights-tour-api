d7d3ed789085463baa4566aa72fe40a3eb9c2f16
tapanr97 <tapanr97@gmail.com>
2018-09-26 12:21:29 +0530

    added cpp file

--- server.js
+++ server.js
@@ -6,13 +6,7 @@ const fs = require('fs');
 const app = express();
 const x=1;
 app.use(bodyParser.json());
-// app.use(function (req, res, next) {
-//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET');
-//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-//   res.setHeader('Access-Control-Allow-Credentials', true);
-//   next();
-// });
+
 app.use(cors({
   origin: true,
   exposedHeaders: ['Content-Length', 'X-Foo', 'X-Bar'],
@@ -29,20 +23,14 @@ int main(){
 
 var trace;
 
-// Handle / route
 app.post('/', (req, res) => {
 
-  console.log(req.body.x);
-//  res.send(req.dimensions);
   let inp = req.body.x + " " + req.body.y + " " + req.body.dimensions;
-  console.log(inp);
   cpp.runFile('LOI-code2.cpp', {stdin: inp, compileTimeout:10000}, (err, result) => {
     if(err){
         console.log(err);
     }
     else{
-    //      console.log("HI");
-    //      console.log(result.stdout);
           trace = result.stdout;
           fs.writeFile(__dirname + '/' + "xyz.txt", result.stdout, function (err) {
             if (err) {
@@ -51,8 +39,6 @@ app.post('/', (req, res) => {
             console.log('Access control JSON file has been saved.');
           });
           let out = trace.split("\n");
-          console.log(out[0]);
-
           res.status(200).json({
             ans: out
           });
